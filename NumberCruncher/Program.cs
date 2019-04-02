d92d51082c5d68d2da373d0a3be2deb2ef73ed19
carrieheyen@gmail.com <carrieheyen@gmail.com>
2019-04-01 19:07:59 -0500

    Finished

--- NumberCruncher/Program.cs
+++ NumberCruncher/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace NumberCruncher
 {
@@ -6,6 +8,17 @@ namespace NumberCruncher
     {
         public static void Main()
         {
+            List<int> numbers = new List<int>();
+            int number = 0;
+            for (int i = 0; i < 20; i++)
+            {
+                number = new Random().Next(1, 100);
+                numbers.Add(number);
+            }
+            
+            
+            Console.WriteLine(NumberUtility.NumberUtility.SumOfEvenNumbers(numbers));
+
             //Create a list of ints 
             //Call SumOfEvenNumbers in NumberUtility and pass this list in
             //Pring out the result
--- NumberUtility/NumberUtility.cs
+++ NumberUtility/NumberUtility.cs
@@ -8,5 +8,22 @@ namespace NumberUtility
     {
         //create a static method SumOfEvenNumbers 
         //it should take a list of ints and returns the sum of all even number in that list
+        public static int SumOfEvenNumbers(List<int> numbers)
+        {
+            int sumOfInts = 0;
+            List<int> evenNumbers = new List<int>();
+            foreach (int number in numbers)
+            {
+                if (number % 2 == 0)
+                {
+                    evenNumbers.Add(number);
+                }
+            }
+            foreach (int evenNumber in evenNumbers)
+            {
+                sumOfInts += evenNumber;
+            }
+            return sumOfInts;
+        }
     }
 }
